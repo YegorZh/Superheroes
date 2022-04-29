93985341383edbc71a6c7ac3a5db1adef6d23473
Yegor <ultrahentaiturbolord@gmail.com>
2022-04-29 12:28:28 +0300

    Added fonts

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,13 @@ import './index.css';
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
+const appHeight = () => {
+  const doc = document.documentElement;
+  doc.style.setProperty('--app-height', `${window.innerHeight}px`);
+};
+window.addEventListener('resize', appHeight);
+appHeight();
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
