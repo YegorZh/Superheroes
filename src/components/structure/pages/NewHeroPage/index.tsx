7a1eac2455c9a2d2388dd3653e45c80cf1076270
Yegor <ultrahentaiturbolord@gmail.com>
2022-04-30 18:15:09 +0300

    Axios posts added heroes

--- src/components/structure/pages/NewHeroPage/index.tsx
+++ src/components/structure/pages/NewHeroPage/index.tsx
@@ -1,4 +1,6 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { Navigate } from 'react-router-dom';
 import {
   validateImageLink,
   validateImages,
@@ -43,9 +45,10 @@ const NewHeroPage: React.FC = () => {
   ];
 
   const [newHero, setNewHero] = useState<Hero>({ nickname: '', images: [] });
-  const [newImage, setNewImage] = useState('');
-  const [imageAdded, setImageAdded] = useState(false);
+  const [newImage, setNewImage] = useState<string>('');
+  const [imageAdded, setImageAdded] = useState<boolean>(false);
   const [error, setError] = useState<string>('');
+  const [id, setId] = useState<string>('');
 
   const onChangeHandler = (key: string, value: string) => {
     setNewHero((oldHero) => {
@@ -72,6 +75,13 @@ const NewHeroPage: React.FC = () => {
       validateNickname(newHero.nickname) || validateImages(newHero.images);
     if (error) return setError(error);
     setError('');
+    axios
+      .post('https://ninjas-api.herokuapp.com/heroes', newHero)
+      .then((response) => {
+        console.log(response);
+        if (response.status === 200) setId(response.data._id);
+      })
+      .catch((err) => setError(err.response?.data?.error || err.message));
   };
 
   useEffect(() => {
@@ -80,6 +90,7 @@ const NewHeroPage: React.FC = () => {
 
   return (
     <div className="my-8 flex w-full flex-col gap-3 px-12">
+      {id && <Navigate to={`/heroes/${id}`} />}
       <div className="max-w-lg space-y-3">
         {fields.map((field, i) =>
           field.textfield ? (
